2cd4a56c9b0978be1e9a823b0d75bcae01588964
Lucas Dobbins <lucas@luderemedia.com>
2011-12-09 13:24:54 +1100

    Changed spec to reflect changes to paperclip validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,18 +13,18 @@ describe Product do
     end
     
     it "should not be valid with only image_url" do
-      @product.image_url = "some image url"
+      @product.image_file_name = "some image url"
       @product.should_not be_valid
     end
     
     it "should not be valid with only pdf_url" do
-      @product.pdf_url = "some pdf url"
+      @product.pdf_file_name = "some pdf url"
       @product.should_not be_valid
     end
     
     it "should be valid with image_url, pdf_url, title & at least 1 spec" do
-      @product.image_url = "Some image url"
-      @product.pdf_url = "some pdf url"
+      @product.image_file_name = "Some image url"
+      @product.pdf_file_name = "some pdf url"
       @product.title = "foo cam"
       @product.specs << Spec.new(:title => "Shutter", :value => "fast", :product => @product)
       
