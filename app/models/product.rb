b4994404c45914edf6d2d44b5f09c037b70ed88e
Lucas Dobbins <lucas@luderemedia.com>
2012-03-19 22:49:02 +1100

    WIP

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,19 +4,24 @@ class Product < ActiveRecord::Base
   belongs_to :sub_category
 
   validates_presence_of :image_file_name, :pdf_file_name, :title, :category
+  
+  validates_attachment_content_type :pdf, :content_type =>['application/pdf']
 
   has_attached_file :image, 
   :styles => { 
     :standard => "<455x190>", 
     :thumb => "<290x140>" 
-  },
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml",
-  :path => "/:style/:id/:filename"
+    },
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => "/:style/:id/:filename"
 
-  has_attached_file :pdf
+    has_attached_file :pdf,
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root.to_s}/config/s3.yml",
+    :path => "/userpdfs/:id/:basename.:extension"
 
-  delegate :sub_category_name, :to => :sub_category
+    delegate :sub_category_name, :to => :sub_category
 
 
-end
+  end
