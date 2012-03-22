0c05a18314309ad764578bec4c458a0c1ce1236f
Lucas Dobbins <lucas@luderemedia.com>
2012-03-22 14:12:16 +1100

    Readded event handler JS WIP

--- app/assets/javascripts/event_handlers.js
+++ app/assets/javascripts/event_handlers.js
@@ -1,14 +1,14 @@
-// $(function() {
-// 
-//  $("select#product_category_id").change(function(){
-//    var category_name = this.options[1].text
-//    if (category_name === "Jibs & Cranes") {
-//      $("li#product_sub_category_input").css("visibility", "visible");
-//      $("select#product_sub_category_id").attr("disabled", false);
-//    }else{
-//      $("li#product_sub_category_input").css("visibility", "hidden");
-//      $("select#product_sub_category_id").attr("disabled", true); 
-//    };
-//  });
-// 
-// });
+$(function() {
+
+	$("select#product_category_id").change(function(){
+		var category_name = this.options[1].text
+		if (category_name === "Jibs & Cranes") {
+			$("li#product_sub_category_input").css("visibility", "visible");
+			$("select#product_sub_category_id").attr("disabled", false);
+		}else{
+			$("li#product_sub_category_input").css("visibility", "hidden");
+			$("select#product_sub_category_id").attr("disabled", true);	
+		};
+	});
+
+});
